48e150662381c6e43d356db9d3d8f3aebf58df3b
Astolfo34 <jsebasdroid31@gmail.com>
2025-10-08 09:43:08 -0500

    |----| Configuracion de los Cors Globales para elegir el fronted local o desplegado utilizado y recibir solicitudes de ambos |----|

--- product-demoLogin/src/main/java/com/jsebastian/eden/EdenSys/config/WebConfig.java
+++ product-demoLogin/src/main/java/com/jsebastian/eden/EdenSys/config/WebConfig.java
@@ -25,7 +25,7 @@ public class WebConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/**")
+                registry.addMapping("/api/auth/**")
                         .allowedOrigins(frontendUrl, frontendLocalUrl)
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowCredentials(true);
