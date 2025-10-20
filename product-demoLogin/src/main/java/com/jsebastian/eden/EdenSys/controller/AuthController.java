7cafafe24b32dfd1582e6fc75f8261fa59dabfd9
Diego Nicolás Peña Rincon; Escritorio. <diegonicolaspenarincon@gmail.com>
2025-10-20 01:37:48 -0500

    se cambio el docker file

--- product-demoLogin/src/main/java/com/jsebastian/eden/EdenSys/controller/AuthController.java
+++ product-demoLogin/src/main/java/com/jsebastian/eden/EdenSys/controller/AuthController.java
@@ -7,7 +7,7 @@ import com.jsebastian.eden.EdenSys.services.interfaces.UserService;
 import com.jsebastian.eden.EdenSys.services.JwtService;
 import com.jsebastian.eden.EdenSys.exceptions.ValueConflictException;
 import lombok.RequiredArgsConstructor;
-import lombok.Value;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
